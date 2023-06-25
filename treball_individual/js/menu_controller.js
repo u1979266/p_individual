da80c0b5c53f64fdb895125ce17a884123271f1a
ElPolGuai <polcomiporcar@gmail.com>
2023-06-25 20:50:33 +0200

    menu principal amb vue

--- treball_individual/js/menu_controller.js
+++ treball_individual/js/menu_controller.js
@@ -1,19 +1,32 @@
-function start_game(){
-	name = prompt("User name");
-	
-	sessionStorage.setItem("username", name);
-	
-	loadpage("./mode1/game.html");
-}
-
-function options(){
-	loadpage("./mode1/options.html");
-}
-
-function exit() {
-	if (name != ""){
-		alert("Leaving " + name + "'s game");
+var menu = new Vue({
+	el: "#menu_id",
+	methods: {
+		start_game(mode) {
+			sessionStorage.clear();
+			name = prompt("User name");
+			sessionStorage.setItem("username", name);
+			
+			if (mode == "standard") loadpage("html/standard.html");
+			else if (mode == "infinite") loadpage("html/infinite.html");
+			//Podria haver fet un loadpage("html/" + mode + ".html") o algo aixi però per simplicitat i perquè no depengui del nom del html 
+			//(ja que per qualsevol motiu si vulgués seguir en el projecte i decidís canviar noms dels html no tingui problemes.)
+		},
+		load() {
+			loadpage("html/load.html");
+		},
+		options() {
+			loadpage("options.html");
+		},
+		scoreboard(){
+			loadpage("html/scoreboard.html");
+		},
+		exit() {
+			if (name != ""){
+				alert("Leaving " + name + "'s game");
+			}
+			name = "";
+			localStorage.clear();
+			loadpage("../index.html");
+		}
 	}
-	name = "";
-	loadpage("../index.html")
-}
+});
